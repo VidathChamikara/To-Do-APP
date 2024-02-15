ebd4e49ae10fcf89b9cfa697d3f03f8a1871e973
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-02-15 12:28:44 +0530

    Complete frontend

---
+++ frontend/src/components/ToDo.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React from "react";
+import { AiFillEdit } from "react-icons/ai";
+import { RxCross1 } from "react-icons/rx";
+import { baseURL } from "../utils/constant";
+
+const ToDo = ({ text, id, setUpdateUI, setShowPopup, setPopupContent }) => {
+  const deleteTodo = () => {
+    axios.delete(`${baseURL}/delete/${id}`).then((res) => {
+      console.log(res.data);
+      setUpdateUI((prevState) => !prevState);
+    });
+  };
+
+  const updateToDo = () => {
+    setPopupContent({ text, id });
+    setShowPopup(true);
+  };
+
+  return (
+    <div className="toDo">
+      {text}
+      <div className="icons">
+        <AiFillEdit className="icon" onClick={updateToDo} />
+        <RxCross1 className="icon" onClick={deleteTodo} />
+      </div>
+    </div>
+  );
+};
+
+export default ToDo;
